2495b800158971f25466ed9fe0b233d7e20cd897
Mahemo <mario.hemo2004@gmail.com>
2026-07-27 11:53:46 -0600

    fix: removed next.config.ts redirect

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['zerogate.local'],
-  async redirects() {
-    return [
-      {
-        source: '/generate_204',
-        destination: '/login',
-        permanent: false,
-      },
-      {
-        source: '/gen_204',
-        destination: '/login',
-        permanent: false,
-      }
-    ];
-  },
 };
 
 module.exports = nextConfig;
